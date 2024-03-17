824595a6b889d14302eef3f08403dd647e2237a5
G2Squared <gesket127@gmail.com>
2024-03-17 00:19:54 -0500

    Updating pages

---
+++ src/components/contentPages/AboutMe.jsx
@@ -0,0 +1,17 @@
+function AboutMe() {
+
+    return (
+      <section class="flexRow"> 
+      <h1 id="about-me" class="flexSmallItem">About Me</h1>
+        <section class="flexBigItem"> 
+          <section class="flexAboutMe">
+              <p>Allow me to introduce myself. My name is Gary Harris Jr and I am attending Washington University St. Louis for a Full Stack Coding Bootcamp. I am currently in the process of switching careers from healthcare to tech.</p>
+          </section>
+        </section>
+      </section>
+      );
+
+
+}
+
+export default AboutMe;
